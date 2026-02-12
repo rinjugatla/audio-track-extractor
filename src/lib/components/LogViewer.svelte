c189e806fb1f75e4a0aad85fa5098190d690475a
rinjugatla <rin.jugatla@gmail.com>
2026-02-13 00:04:33 +0900

    feat: ログ更新時に最新行に自動スクロールする機能を追加

--- src/lib/components/LogViewer.svelte
+++ src/lib/components/LogViewer.svelte
@@ -4,12 +4,32 @@
 	}
 
 	let { logs }: Props = $props();
+
+	let container = $state<HTMLDivElement>();
+	let isSticky = $state(true);
+
+	function onScroll() {
+		if (!container) return;
+		const { scrollTop, scrollHeight, clientHeight } = container;
+		isSticky = scrollHeight - (scrollTop + clientHeight) < 20;
+	}
+
+	$effect(() => {
+		logs;
+		if (isSticky && container) {
+			container.scrollTop = container.scrollHeight;
+		}
+	});
 </script>
 
 {#if logs.length > 0}
-	<div class="mockup-code mt-6 max-h-40 w-full overflow-y-auto text-left text-xs">
+	<div
+		bind:this={container}
+		onscroll={onScroll}
+		class="mockup-code mt-6 max-h-32 w-full overflow-y-auto text-left text-xs"
+	>
 		{#each logs as log, i (i)}
-			<pre data-prefix=">"><code>{log}</code></pre>
+			<pre data-prefix=">" class="whitespace-pre-wrap wrap-break-words"><code>{log}</code></pre>
 		{/each}
 	</div>
 {/if}
