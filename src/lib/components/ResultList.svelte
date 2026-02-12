7f0156c6ef8a159c683f0b91b3a163990119abe5
rinjugatla <rin.jugatla@gmail.com>
2026-02-13 01:51:06 +0900

    fix: aac形式の場合にファイル名に拡張子が付与されない問題を修正

--- src/lib/components/ResultList.svelte
+++ src/lib/components/ResultList.svelte
@@ -26,16 +26,17 @@
 		</h3>
 
 		{#each extractedTracks as track (track.name)}
+			{@const extension = track.name.slice(track.name.lastIndexOf('.'))}
 			<div class="card w-full bg-base-200 p-4 shadow-sm">
 				<h4 class="mb-2 text-left font-bold">{track.label}</h4>
 				<audio controls src={track.url} class="mb-2 w-full"></audio>
 				<div class="flex justify-end">
 					<a
 						href={track.url}
-						download={`${filenameBase}${track.number}`}
+						download={`${filenameBase}${track.number}${extension}`}
 						class="btn btn-outline btn-sm btn-secondary"
 					>
-						Download {`${filenameBase}${track.number}`}
+						Download {`${filenameBase}${track.number}${extension}`}
 					</a>
 				</div>
 			</div>
