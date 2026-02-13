599564f08bdf027ba77f3c14d8768fe9ad69ef43
rinjugatla <rin.jugatla@gmail.com>
2026-02-13 12:29:08 +0900

    feat: フッターを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+<Footer />
