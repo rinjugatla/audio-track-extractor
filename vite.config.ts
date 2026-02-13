201ed1db2bcd59dfa60ce081e8b19ea4c0f0da08
rinjugatla <rin.jugatla@gmail.com>
2026-02-13 18:11:51 +0900

    feat: 多言語対応のためのパッケージを追加、初期設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglide } from "@inlang/paraglide-js-adapter-vite"
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), paraglide({
+		project: "./project.inlang",
+		outdir: "./src/paraglide",
+	})],
 	server: {
 		headers: {
 			'Cross-Origin-Opener-Policy': 'same-origin',
